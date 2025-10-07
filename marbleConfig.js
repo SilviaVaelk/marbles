4d4cf4126bdf4799074255be803c158a77375fe1
SilviaVaelk <silvia.vaelk@gmail.com>
2025-10-07 18:42:35 +0100

    Update marbleConfig.js

--- marbleConfig.js
+++ marbleConfig.js
@@ -4,23 +4,33 @@ export const MARBLE_CONFIGS = [
     color: '#d9d9ff',
     glb: 'assets/inner-model.glb',
     link: 'https://example.com/project1',
-    size: 1
+    size: 1,
+    materialOptions: {
+      transmission: 0.9,
+      opacity: 0.85,
+      thickness: 2.0
+    }
   },
   {
     color: '#ffeedd',
     glb: 'assets/inner-model-5.glb',
     link: 'https://example.com/project2',
-    size: 1.2
+    size: 1.2,
+    materialOptions: {
+      transmission: 0.95,
+      opacity: 0.8,
+      thickness: 2.5
+    }
   },
   {
-    color: '#92F5B5',
+    color: '#FF5C5C',
     glb: null,
     link: 'https://example.com/project3',
     size: 0.7,
     materialOptions: {
-      transmission: 0.95,
-      opacity: 0.8,
-      thickness: 4.5
+      transmission: 0.5,
+      opacity: 0.9,
+      thickness: 1.5
     }
   }
 ];
