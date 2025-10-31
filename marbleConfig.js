bb08df9a71bdea2a14065bbc6a012e77cf49e02f
SilviaVaelk <silvia.vaelk@gmail.com>
2025-10-31 15:23:59 +0000

    Update marbleConfig.js

--- marbleConfig.js
+++ marbleConfig.js
@@ -2,8 +2,7 @@
 export const MARBLE_CONFIGS = [
   {
     title: "[Digital painting]",
-    description: "Blabla",
-    link: 'https://example.com/project1',
+    description: "When I'm left unsupervised with a colour picker. <a href="https://www.silviavaelk.com/digital-painting" target="_blank">Click here</a>.",
     color: '#d9d9ff',
     glb: 'assets/inner-model.glb',
     size: 1,
