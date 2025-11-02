b9f7b83b6292d8c3b618245ae261c2eadbdaeb96
SilviaVaelk <silvia.vaelk@gmail.com>
2025-11-02 17:01:49 +0000

    Update marbleConfig.js

--- marbleConfig.js
+++ marbleConfig.js
@@ -2,7 +2,7 @@
 export const MARBLE_CONFIGS = [
   {
     title: "[Digital painting]",
-    description: 'When left unsupervised with a colour picker. <a href="https://www.silviavaelk.com/digital-painting" target="_blank">Click here</a>.',
+    description: 'When left unsupervised with a colour picker. <a href="https://www.silviavaelk.com/digital-painting" target="_blank">Take a look</a>.',
     color: '#d9d9ff',
     lightColor: 0xff66cc,
     glb: 'assets/inner-model.glb',
@@ -19,8 +19,7 @@ export const MARBLE_CONFIGS = [
   
   {
     title: "[Experiments]",
-    description: "Snippets with Cavalry, Blender, etc.",
-    link: 'https://example.com/project2',
+    description: 'Exploring visuals and motion through trial and error. <a href="https://www.silviavaelk.com/experiments" target="_blank">Click here</a>.',
     color: '#ffffff',
     lightColor: 0xff66cc,
     glb: 'assets/inner-model-5.glb',
@@ -29,28 +28,27 @@ export const MARBLE_CONFIGS = [
       transmission: 0.95,
       opacity: 0.8,
       thickness: 2.5,
-      roughness: 0.95,
-      clearcoat: 1.0,
+      roughness: 0.5,
+      clearcoat: 2.0,
       clearcoatRoughness: 0.01
     }
   },
   
   {
     title: "[Slit-scanner]",
-    description: "Experimental tool to achieve the slit-scan effect",
-    link: 'https://example.com/project3',
+    description: 'Experimental tool to play around with time and motion distortion. <a href="https://www.silviavaelk.com/slit-scanner" target="_blank">Try it out</a>.',
     texture: 'assets/marble-pattern-spiral-abstract.png',
     color: '#ff99cc',
     lightColor: 0xff66cc,
     glb: null,
     size: 0.6,
     materialOptions: {
-      transmission: 0.3,
-      opacity: 0.9,
-      thickness: 5,
-      roughness: 0.05,
-      clearcoat: 1.0,
-      clearcoatRoughness: 0.01
+      transmission: 0.1,
+      opacity: 1.0,
+      thickness: 0.5,
+      roughness: 0.1,
+      clearcoat: 0.5,
+      clearcoatRoughness: 0.2
     }
   }
 ];
