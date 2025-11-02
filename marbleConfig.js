d93aa7388cac7f1d98634f07fc73dfd668c9c173
SilviaVaelk <silvia.vaelk@gmail.com>
2025-11-02 17:05:16 +0000

    Update marbleConfig.js

--- marbleConfig.js
+++ marbleConfig.js
@@ -2,7 +2,7 @@
 export const MARBLE_CONFIGS = [
   {
     title: "[Digital painting]",
-    description: 'When left unsupervised with a colour picker. <a href="https://www.silviavaelk.com/digital-painting" target="_blank">Take a look</a>.',
+    description: 'When left unsupervised with a colour picker. <a href="https://www.silviavaelk.com/digital-painting" target="_blank">Take a look</a>',
     color: '#d9d9ff',
     lightColor: 0xff66cc,
     glb: 'assets/inner-model.glb',
@@ -19,7 +19,7 @@ export const MARBLE_CONFIGS = [
   
   {
     title: "[Experiments]",
-    description: 'Exploring visuals and motion through trial and error. <a href="https://www.silviavaelk.com/experiments" target="_blank">Click here</a>.',
+    description: 'Exploring visuals and motion through trial and error. <a href="https://www.silviavaelk.com/experiments" target="_blank">Click here</a>',
     color: '#ffffff',
     lightColor: 0xff66cc,
     glb: 'assets/inner-model-5.glb',
@@ -36,7 +36,7 @@ export const MARBLE_CONFIGS = [
   
   {
     title: "[Slit-scanner]",
-    description: 'Experimental tool to play around with time and motion distortion. <a href="https://www.silviavaelk.com/slit-scanner" target="_blank">Try it out</a>.',
+    description: 'Experimental tool to play around with time and motion distortion. <a href="https://www.silviavaelk.com/slit-scanner" target="_blank">Try it</a>',
     texture: 'assets/marble-pattern-spiral-abstract.png',
     color: '#ff99cc',
     lightColor: 0xff66cc,
@@ -47,8 +47,8 @@ export const MARBLE_CONFIGS = [
       opacity: 1.0,
       thickness: 0.5,
       roughness: 0.1,
-      clearcoat: 0.5,
-      clearcoatRoughness: 0.2
+      clearcoat: 1.5,
+      clearcoatRoughness: 0.1
     }
   }
 ];
